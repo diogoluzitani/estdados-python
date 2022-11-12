9e2854f178dc57ea8e629c72dfb3ed16cf7c9243
Diogo Luzitani <dyoluzitani@gmail.com>
2022-11-12 17:34:57 -0400

    Arvore binaria de busca

---
+++ abb.py
@@ -0,0 +1,100 @@
+class celula:
+    def __init__(self, valor, FE=None, FD=None):
+        self.valor = valor
+        self.fe = FE
+        self.fd = FD    
+        
+    def __str__(self):
+        return "No("+str(self.valor)+")"
+
+class ABB:
+    def __init__(self, raiz):
+        self.raiz = raiz
+
+    def teste():
+        raiz = celula(4)
+        n2 = celula(3)
+        n3 = celula(5)
+
+
+
+
+
+
+
+def busca(r, nova):
+    atual = r
+                
+    while (atual.valor != None):
+        if (nova < atual.valor) and (atual.fe != None):
+                atual = atual.fe
+        elif(nova > atual.valor) and (atual.fd != None):
+                atual = atual.fd
+        else:
+            return "No("+str(atual.valor)+")"
+
+
+r = celula(7)
+n1 = celula(3)
+n2 = celula(1)
+n3 = celula(4)
+n4 = celula(11)
+
+r.fe = n1
+n1.fe = n2
+n1.fd = n3
+r.fd = n4
+
+nova = celula(int(input()))
+
+p = busca(r, nova.valor)
+
+print(p)
+
+------
+
+class celula:
+    def __init__(self, valor, FE=None, FD=None):
+        self.valor = valor
+        self.fe = FE
+        self.fd = FD    
+        
+    def __str__(self):
+        return "No("+str(self.valor)+")"
+
+def busca(r, nova):
+    atual = r
+                
+    while (atual.valor != None):
+        if (nova < atual.valor) and (atual.fe != None):
+                atual = atual.fe
+        elif(nova > atual.valor) and (atual.fd != None):
+                atual = atual.fd
+        else:
+            return atual
+
+def inserir(r, valor):
+    pai = busca(r, valor)
+    
+    if(valor > pai.valor):
+        pai.fd=celula(valor)
+    else:
+        pai.fe=celula(valor)
+    
+    return pai
+
+
+r = celula(7)
+n1 = celula(3)
+n2 = celula(1)
+n3 = celula(4)
+n4 = celula(11)
+
+r.fe = n1
+n1.fe = n2
+n1.fd = n3
+r.fd = n4
+
+nova = celula(int(input()))
+
+p = inserir(r, nova.valor)
